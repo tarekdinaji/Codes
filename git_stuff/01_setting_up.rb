ea1293e9dabdf2daedbc914fecad152ecfeef4e0
Tarek Hossain <mtarekhossain@gmail.com>
2021-11-07 23:57:43 +0600

    daily commit

--- git_stuff/01_setting_up.rb
+++ git_stuff/01_setting_up.rb
@@ -18,4 +18,24 @@ git init
 
 git clone git@github.com:xyzabcd1234 # 
 
+# now move your data to that folder
+# use the following command to check the status 
+git status 
+
+# Now we want the newly added folder/files and push it to the online at github
+# To do that we need to add those newly added files/folders to the staging
+git add --all # OR git add -A
+# To revert back from stage to back use
+git reset
+# To add only the corrent directory to stage use
+git add . 
+# To commit the changes use 
+git commit -m "November Commit"
+# To rollback from commit use
+git reset HEAD~
+
+# Branching
+
+# Pushing to remote
+git push origin main  # Here origin means remote and main means main branch
 
